0a6e2e15b5213aaee458e61631204e6232242867
ViniSSantos <cepef.rh@gmail.com>
2023-02-09 21:33:22 -0300

    study

---
+++ 16For.js
@@ -0,0 +1,44 @@
+        //For   Estrutura de Repetição
+/*
+For é umas das Est.de.Rep. mais usada
+
+Tem base em 3 requesitos
+1 - Ponto de Partida
+2 - Ponto de Parada
+3 - Incremento (adicionar + 1 dele a ele mesmo)
+
+obs: i++ é = a:     i = i + 1; 
+*/ 
+
+//var conta = 0;
+
+for (var conta = 0; conta <= 100; conta++) {
+    console.log(conta);
+}
+
+
+for(var zero = 0; zero <= 100; zero++){
+    console.log(zero);    
+}
+
+/*
+Pt Inicio:    0
+Pt Parada:    é <= 100
+Incremento:   ele++
+*/
+for(i = 0; i <= 100; i++){
+    console.log(i);
+}
+
+
+//Desafio contagem regressiva de 0 100 com For
+
+//Adiciona = Incremntar     i++
+//Subtrair = Decrementar    i--
+
+
+//P.In: 100   PtPard: 0     decrem: var--
+for(num = 100; num >= 0;    num--){
+   
+    console.log(num);
+}
